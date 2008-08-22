7dbe132e8a4f2afe7a0f5f1d5a1ae8ffccf55315
Sander Hartlage <sander.hartlage@gmail.com>
2008-08-22 14:19:14 +0900

    Removed some testing files.

--- dump.rb
+++
@@ -1,9 +0,0 @@
-app = Growl.application do |a|
-  a.name = "Testo"
-  a.app_icon = "Mail"
-  a.notification do |n|
-    n.name = "Yo Mommma"
-    n.title = "Some Things About Yo Momma"
-    n.message = "Yo momma is so {adj}, when she {v}, {pl}!"
-  end
-end
